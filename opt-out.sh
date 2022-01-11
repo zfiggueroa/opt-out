70f5df80b8d229efe51a9235352ca2bfeb00c089
zfiggueroa <95833594+zfiggueroa@users.noreply.github.com>
2022-01-11 13:20:10 -0700

    Update opt-out.sh

--- opt-out.sh
+++ opt-out.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 printf "\033c\033]2;opt-out\a"
-#setterm -cursor off
+setterm -cursor off
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-video_path=("$SCRIPT_DIR")
+video_path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # mapfile
-mapfile -t youtube	< "$video_path/data/yt.users"
-mapfile -t tiktok	< "$video_path/data/tt.users"
+mapfile -t youtube	< "$video_path/yt.users"
+mapfile -t tiktok	< "$video_path/tt.users"
 
 # options
-loging=" -ciw --playlist-end 5 --dateafter now-2days --mark-watched --download-archive"
+loging=" -ciw --playlist-end 10 --dateafter now-2days --mark-watched --download-archive"
 secure=" --no-warnings --no-part --no-cache-dir --no-call-home -4 --geo-bypass --geo-bypass-country NL --restrict-filenames"
 
-# re-populate yt history
+# re-build yt history
 if [[ $1 = "hs" ]]; then
-	dd if=/dev/null of="$video_path/data/yt.history"; clear
-	for i in "${youtube[@]}"; do yt-dlp -f wa $i --playlist-end 5 --mark-watched --download-archive "$video_path/data/yt.history"\
-	--output "$video_path/null"; clear; done; rm "$video_path/null"; exit
-else break 2>/dev/null; fi
+dd if=/dev/null of="$video_path/yt.history"; clear
+	for i in "${youtube[@]}"; do
+	yt-dlp -f wa $i $secure $loging "$video_path/yt.history" --output "$video_path/.null"; clear; done; rm "$video_path/.null"; exit
+break; fi
 
 # tiktok
 if [[ $1 = "tt" ]]; then
 	for i in "${tiktok[@]}"; do
-	yt-dlp -f b $i --output "$video_path/downloaded-video/TikTok/%(id)s.%(ext)s"\
-	$loging "$video_path/data/tt.history" $secure; clear; done; exit
-else break 2>/dev/null; fi
+	yt-dlp -f b $i $secure $loging "$video_path/tt.history" --output "$video_path/downloaded-videos/TikTok/%(id)s.%(ext)s"; clear; done; exit
+break; fi
 
 # youtube
 if [[ $1 = "yt" ]]; then
 for i in "${youtube[@]}"; do
-	yt-dlp -f 'bv*[height<=480]+ba/b[height<=480] / wv*+ba/w' $i --output "$video_path/downloaded-video/%(uploader)s/[%(upload_date)s] %(title)s.%(ext)s"\
-	$loging "$video_path/data/yt.history" $secure --match-filter '!is_live' --youtube-skip-dash-manifest; clear; done; exit
-else break 2>/dev/null; fi
+	yt-dlp -f 'bv*[height<=480]+ba/b[height<=480] / wv*+ba/w' $i $secure $loging "$video_path/yt.history" --match-filter '!is_live'\
+	--output "$video_path/downloaded-videos/%(uploader)s/[%(upload_date)s] %(title)s.%(ext)s"; clear; done; exit
+break; fi
 
-if [[ $1 != "hs" ]] || [[ $1 != "tt" ]] || [[ $1 != "yt" ]]; then printf "\ninvalid argument option\n\n"; fi
+# verify arguments
+if [[ $1 != "hs" || $1 != "tt" || $1 != "yt" ]]; then printf "\ninvalid argument option\n\n"; fi
